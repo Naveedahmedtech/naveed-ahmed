04f017a0bbd451d6357be2c2849c7cd6416df541
Naveedahmedtech <technaveedahmed@gmail.com>
2024-03-18 19:48:06 +0500

    project card color modified

--- src/pages/home/components/ProjectCard.tsx
+++ src/pages/home/components/ProjectCard.tsx
@@ -10,10 +10,10 @@ const ProjectCard = ({ project }: any) => {
         // key={index}
         >
             <img src={project.image} alt={project.name} className="w-full h-64 object-cover" loading="lazy" />
-            <div className="absolute inset-0 bg-black bg-opacity-60 opacity-0 transition-opacity duration-500 hover:opacity-100 flex items-center justify-center p-6">
+            <div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 transition-opacity duration-500 hover:opacity-100 flex items-center justify-center p-6">
                 <div>
-                    <h3 className="mb-2 text-2xl font-semibold text-text">{project.name}</h3>
-                    <p className="mb-4 text-lg text-text">{project.description}</p>
+                    <h3 className="mb-2 text-2xl font-semibold text-white">{project.name}</h3>
+                    <p className="mb-4 text-lg text-white">{project.description}</p>
                     <a href={project.demoUrl} target="_blank" rel="noopener noreferrer" className="inline-block bg-primary text-white font-bold py-2 px-4 rounded hover:bg-primary-dark transition duration-300">
                         View Project
                     </a>
